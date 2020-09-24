ae2214012da06727bb64b837d65a523e6fd945f0
marcosfp <marcosfppf@gmail.com>
2020-09-25 00:08:50 +0200

    Primer Commit

---
+++ demo/src/main/java/des/demo/ControladorFormulario.java
@@ -0,0 +1,32 @@
+package des.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ControladorFormulario {
+
+	@RequestMapping(value="/formulario", method=RequestMethod.GET)
+	public String hola_get() {
+
+		return "formulario";
+	}
+
+	@RequestMapping(value="/formulario", method=RequestMethod.POST)
+	public String hola_post(Model modelo,
+			@RequestParam String nombre,
+			@RequestParam Integer nacido,
+			@RequestParam String hm) {
+
+		modelo.addAttribute("nombre_form", nombre);
+		modelo.addAttribute("nacido_form,", nacido);
+		modelo.addAttribute("hm_form", hm);
+
+
+		return "datosFormulario";
+	}
+	
+}
---
+++ demo/src/main/java/des/demo/ControladorHola.java
@@ -0,0 +1,17 @@
+package des.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ControladorHola {
+
+	@RequestMapping("/hola")
+	public String hola(Model modelo) {
+
+		modelo.addAttribute("mensaje", "hola desde thymeleaf");
+		return "hola";
+	}
+
+}
